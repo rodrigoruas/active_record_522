627c7e6d016d4cc61f966f50ac70cb8f9bd63659
Sebastien Saunier <seb@saunier.me>
2014-10-25 14:03:44 +0200

    Add a logger

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,13 @@ require 'yaml'
 
 # This is some boilerplate code to read the config/database.yml file
 # And connect to the database
-
 config_path = File.join(File.dirname(__FILE__), "database.yml")
 ActiveRecord::Base.configurations = YAML.load_file(config_path)
 ActiveRecord::Base.establish_connection(:development)
+
+# Set a logger so that you can view the SQL actually performed by ActiveRecord
+logger = Logger.new(STDOUT)
+logger.formatter = proc do |severity, datetime, progname, msg|
+   "#{msg}\n"
+end
+ActiveRecord::Base.logger = logger
