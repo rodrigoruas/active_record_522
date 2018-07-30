17318a147049c987aee0c84b3c19f579507374d9
Benjamin Baranger <baranger.benjamin@gmail.com>
2018-07-30 10:39:46 +0100

    Revert "active record basics - wrong push"
    This reverts commit f0a40666742b4c092974dd5c8c31cb1cb8603bcf.

--- db/migrate/20180730093213_create_restaurants.rb
+++
@@ -1,9 +0,0 @@
-class CreateRestaurants < ActiveRecord::Migration[5.1]
-  def change
-    create_table :restaurants do |t|
-      t.string    :name
-      t.string    :address
-      t.timestamps # add 2 columns, `created_at` and `updated_at`
-    end
-  end
-end
